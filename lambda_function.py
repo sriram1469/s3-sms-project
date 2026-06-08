d6c0436f895b4b554603971fc2c53c9d7f6722db
sriram1469 <burasriram2002@gmail.com>
2026-06-08 14:19:06 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,11 +4,9 @@ import os
 sns = boto3.client('sns')
 
 def lambda_handler(event, context):
-    # Extract file name from S3 event
     file_name = event['Records'][0]['s3']['object']['key']
     message = f"Your file {file_name} has been uploaded successfully."
     
-    # Publish SMS using sns.publish
     sns.publish(
         PhoneNumber=os.environ['USER_PHONE'],
         Message=message
